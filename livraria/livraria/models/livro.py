39f3b84d15d29b6f090f9e88351abd4c9500b21e
Isadora <106554588+IsadoraAlcantara@users.noreply.github.com>
2024-06-17 16:00:20 -0300

    finalizando seção 14

--- livraria/livraria/models/livro.py
+++ livraria/livraria/models/livro.py
@@ -1,19 +1,6 @@
 from django.db import models
 
-
-class Categoria(models.Model):
-    descricao = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.descricao
-
-
-class Editora(models.Model):
-    nome = models.CharField(max_length=100)
-    site = models.URLField(null=True, blank=True)
-
-    def __str__(self):
-        return self.nome
+from livraria.models import Autor, Categoria, Editora
 
 
 class Livro(models.Model):
