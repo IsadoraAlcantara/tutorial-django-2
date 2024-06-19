9508416a98131e8e4b41970fd1a2172a9f6df972
IsadoraAlcantara <isadoraalca405@gmail.com>
2024-06-18 23:46:20 -0300

    finalização da seção 20

--- livraria/config/urls.py
+++ livraria/config/urls.py
@@ -1,21 +1,15 @@
 from django.contrib import admin
 from django.urls import include, path
 
-from rest_framework.routers import DefaultRouter
-
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
-
 from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularRedocView,
     SpectacularSwaggerView,
 )
+from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-
-from livraria.views import CategoriaViewSet, EditoraViewSet, AutorViewSet, LivroViewSet
+from livraria.views import AutorViewSet, CategoriaViewSet, EditoraViewSet, LivroViewSet
 
 router = DefaultRouter()
 router.register(r"autores", AutorViewSet)
--- livraria/livraria/views/livro.py
+++ livraria/livraria/views/livro.py
@@ -1,7 +1,8 @@
 from rest_framework.viewsets import ModelViewSet
 
 from livraria.models import Livro
-from livraria.serializers import LivroSerializer#, LivroDetailSerializer
+from livraria.serializers import LivroSerializer  # , LivroDetailSerializer
+
 
 class LivroViewSet(ModelViewSet):
     queryset = Livro.objects.all()
